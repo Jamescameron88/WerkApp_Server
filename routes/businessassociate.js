c88338db563033bbd74031e59016e23d8726cddf
James Cameron <58527678+Jamescameron88@users.noreply.github.com>
2022-01-20 13:36:28 -0500

    Revert "Updated route for getting relationship status"

--- routes/businessassociate.js
+++ routes/businessassociate.js
@@ -27,12 +27,17 @@ const user = require("../models/user");
 // @access  PRIVATE (TODO)
 router.post("/AssociateRelationshipStatus", async (req, res) => {
   try {
-    const associationStatus = await models.businessassociate.findOne({
+    const checkAssociationStatus = await models.businessassociate.findOne({
       where: {
         a_Users_UserId: req.body.Self.UserId,
         b_Users_UserId: req.body.ListProfile.UserId
       }
     })
+    if (checkAssociationStatus.length === 0) {
+      associationStatus = {"associationStatus":"NotAssociates"}
+    } else {
+      associationStatus = {"associationStatus":checkAssociationStatus.dataValues.RequestStatus};
+    }
     res.json(associationStatus);
   } catch (err) {
     console.error(err.message);

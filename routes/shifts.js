a195957d73454133d587573b24454578419c659a
Trey <34677838+aulickabw3@users.noreply.github.com>
2022-03-23 14:34:53 -0400

    Updated route to CreateShift and PublishJob

--- routes/shifts.js
+++ routes/shifts.js
@@ -13,8 +13,11 @@ const { sequelize } = require("../models");
 // @access  PRIVATE (TODO)
 router.post("/CreateShift", async (req, res) => {
   
+  var createAShift = [];
+  // var finalResultArray = [];
+
   try { 
-        let createAShift = await models.shifts.findOrCreate({
+        createAShift = await models.shifts.findOrCreate({
           where: {
             ShiftIdentifier: req.body.newShift.ShiftIdentifier,
             DateDay: req.body.newShift.DateDay,
@@ -27,10 +30,11 @@ router.post("/CreateShift", async (req, res) => {
             UserUserId: req.body.newShift.UserUserId,
             NumberOfWerkers: req.body.newShift.NumberOfWerkers
           },
-          
         })
       
-      res.json({createAShift});
+      // console.log(createAShift[0].dataValues.ShiftId);
+
+      res.json({ "ShiftId":createAShift[0].dataValues.ShiftId });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
@@ -39,69 +43,31 @@ router.post("/CreateShift", async (req, res) => {
   
 
 // @route   POST
-// @descr   Create a job shift (line items)
-// @access  PRIVATE (TODO)
-// router.post("/CreateAShift", async (req, res) => {
-//     try {
-//       let createAShift = await models.jobshifts.findOrCreate({
-//         where: {
-//             POCName: req.body.newShift.POCName,
-//             POCPhone: req.body.newShift.POCPhone,
-//             Pay: req.body.newShift.Pay,
-//             StartDateTime: req.body.newShift.StartDateTime,
-//             FinishDateTime: req.body.newShift.FinishDateTime,
-//             ShiftNotes: req.body.newShift.ShiftNotes,
-//             SchedulerApproval: req.body.newShift.SchedulerApproval,
-//             UserUserId: req.body.newShift.UserUserId,
-//             JobJobId: req.body.newShift.JobJobId
-//         }
-//       });
-//       res.json({createAShift});
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send('Server Error');
-//     }
-//   });
-
-
-// @route   POST
-// @descr   Publish a job (make it available) to all associates
+// @descr   Publish a job (make it available) to selected associates
 // @access  PRIVATE (TODO)
 router.post("/PublishJob", async (req, res) => {
     
-  //  have to determine if job is available to:
-  //   - all scheduler's associates
-  //   - a scheduler's crew
-  //   - just certain associates
-  
-  //  All associates
-  //  1 - Get list of all scheduler's associates
   try {
-    let schedulersAssociates = await models.businessassociate.findAll({
-      where: {
-        a_Users_UserId: req.body.schedulerProfile.UserId
-      },
-      attributes: {
-        exclude: ['BusinessAssociateId', 'a_Users_UserId','RequestStatus', 'createdAt', 'updatedAt']
-      }
-    })
   
-//  2 - Publish to those associates
-  for (let i = 0; i < schedulersAssociates.length; i++) {
-    let publishJob = await models.availablejobs.findOrCreate({
+  // console.log(req.body.MyCrew.JobJobID.id);
+  
+  for (let i = 0; i < req.body.MyCrew.Crew.length; i++) {
+    console.log({"crew member":req.body.MyCrew.Crew[i]});
+
+    let publishJob = await models.availableshifts.findOrCreate({
       where: {
-        UserUserId: schedulersAssociates[i].b_Users_UserId,
-        JobJobId: req.body.publishJob.JobJobId
-      }
+        UserUserId: req.body.MyCrew.Crew[i],
+        ShiftShiftId: req.body.MyCrew.JobJobID.id
+      },
     })
   }
 
-      res.json({schedulersAssociates});
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  });
+  res.json({schedulersAssociates});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
   
 
 

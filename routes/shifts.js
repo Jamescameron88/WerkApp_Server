3d35cbb0aaf0862a1b8d97cc0da98360c689b30b
Trey <34677838+aulickabw3@users.noreply.github.com>
2022-03-24 09:35:49 -0400

    Updated the AvailableShifts query

--- routes/shifts.js
+++ routes/shifts.js
@@ -12,10 +12,7 @@ const { sequelize } = require("../models");
 // @descr   Create a job (header)
 // @access  PRIVATE (TODO)
 router.post("/CreateShift", async (req, res) => {
-  
   var createAShift = [];
-  // var finalResultArray = [];
-
   try { 
         createAShift = await models.shifts.findOrCreate({
           where: {
@@ -31,9 +28,6 @@ router.post("/CreateShift", async (req, res) => {
             NumberOfWerkers: req.body.newShift.NumberOfWerkers
           },
         })
-      
-      // console.log(createAShift[0].dataValues.ShiftId);
-
       res.json({ "ShiftId":createAShift[0].dataValues.ShiftId });
     } catch (err) {
       console.error(err.message);
@@ -46,11 +40,7 @@ router.post("/CreateShift", async (req, res) => {
 // @descr   Publish a job (make it available) to selected associates
 // @access  PRIVATE (TODO)
 router.post("/PublishJob", async (req, res) => {
-    
-  try {
-  
-  // console.log(req.body.MyCrew.JobJobID.id);
-  
+  try {  
   for (let i = 0; i < req.body.MyCrew.Crew.length; i++) {
     console.log({"crew member":req.body.MyCrew.Crew[i]});
 
@@ -61,7 +51,6 @@ router.post("/PublishJob", async (req, res) => {
       },
     })
   }
-
   res.json({schedulersAssociates});
   } catch (err) {
     console.error(err.message);
@@ -69,6 +58,55 @@ router.post("/PublishJob", async (req, res) => {
   }
 });
   
+
+//  @route  GET
+//  @descr  Get a list of the jobs that are available for the logged in werker
+//  @access PRIVATE (TODO)
+router.get("/AvailableShifts/:id", async (req, res) => {
+  var availableShiftsArray = [];
+  try {
+    let availableShifts = await models.availableshifts.findAll({
+      where: {
+        UserUserId: req.params.id
+      }, 
+      include: models.shifts
+    });
+
+    console.log(JSON.stringify(availableShifts.length));
+
+    let x = 0;
+    for (let i = 0; i < availableShifts.length; i++) {
+
+      console.log(JSON.stringify(availableShifts[i].Shift.ShiftId));
+
+      let singleShift = await models.usershifts.findAndCountAll({
+        where: {
+          ShiftShiftId: availableShifts[i].Shift.ShiftId
+        }
+      })
+
+      console.log(availableShifts[i].Shift.NumberOfWerkers);
+      console.log(singleShift.count);
+      console.log(availableShifts[i].Shift.NumberOfWerkers - singleShift.count);
+
+      if (availableShifts[i].Shift.NumberOfWerkers - singleShift.count > 0) {
+        console.log("there is an available shift");
+        availableShiftsArray[x] = availableShifts[i];
+        x++;
+      } else {
+        console.log("no shifts available")
+      }
+    }
+
+    console.log(JSON.stringify(availableShiftsArray));
+
+    res.json({availableShiftsArray});
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 
   module.exports = router;

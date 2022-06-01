acdb851213b99b48b819ac640e48485d22a150f1
Trey <34677838+aulickabw3@users.noreply.github.com>
2022-06-01 12:42:23 -0400

    Setup the test config info

---
+++ server.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql2');
+var connection = mysql.createConnection({
+    host:'werkapp-server-v1.czp4jlccvdhe.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'grace_Born00',
+    database: 'werk'
+})
+
+connection.connect(err=>{
+    if (err) throw err;
+    console.log('Connected to DB');
+})
+
+var sql = "Select * from AvailableShifts";
+connection.query(sql, function (err, result) {
+    console.log(result);
+})
